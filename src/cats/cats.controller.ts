62d39c584c279bf764abcfc221277fba3211d565
Sviatlana Mitar <sviatlana.mitar@ee.co.uk>
2023-10-25 16:41:40 +0200

    initial commit

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+import { ListAllCatsDto } from './dto/list-all-cats.dto';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  create(@Body() createCatDto: CreateCatDto) {
+    return this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  findAll(@Query() query: ListAllCatsDto) {
+    console.log(`This action returns all cats (limit: ${query.limit} items)`);
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.catsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    return this.catsService.update(+id, updateCatDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.catsService.remove(+id);
+  }
+}
